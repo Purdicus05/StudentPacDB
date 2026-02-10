72b1865c0ab9b605037f039c5a92bd419e36d43d
Ashley <ashley21_05@hotmail.co.uk>
2026-02-10 15:16:12 +0000

    Begun database foundations

--- PaceAcademyApp/models.py
+++ PaceAcademyApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class PACs(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    department = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Students(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    department = models.CharField(max_length=100)
+    # PAC FK here
+    def __str__(self):
+        return self.name
