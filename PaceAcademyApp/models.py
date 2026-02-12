4c6c431d3285b73aef02167bf4f76deb2c47007e
Ashley <ashley21_05@hotmail.co.uk>
2026-02-12 09:46:50 +0000

    Updated migrations

--- PaceAcademyApp/models.py
+++ PaceAcademyApp/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 
 class PACs(models.Model):
-    firstName = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    email = models.EmailField()
+    firstName = models.CharField(max_length=100, default='')
+    surname = models.CharField(max_length=100, default='')
+    email = models.EmailField(default='')
 
     def __str__(self):
-        return self.name
+        return f'Name: {self.firstName} {self.surname}, Email: {self.email}'
 
 class Students(models.Model):
-    firstName = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    email = models.EmailField()
-    course = models.CharField(max_length=100)
+    firstName = models.CharField(max_length=100, default='')
+    surname = models.CharField(max_length=100, default='')
+    email = models.EmailField(default='')
+    course = models.CharField(max_length=100, default='')
     assignedPAC = models.ForeignKey(PACs, on_delete=models.CASCADE, default = 0) #FK linking to pac table
 
     def __str__(self):
-        return self.name
+        return f'Name: {self.firstName} {self.surname}, Email: {self.email}, Course: {self.course}, Assigned PAC ID: {self.assignedPAC}'
