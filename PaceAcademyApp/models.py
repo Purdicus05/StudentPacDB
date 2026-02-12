1c0853dc149ee0e10872b803c17a553652f53a31
Ashley <ashley21_05@hotmail.co.uk>
2026-02-12 09:36:24 +0000

    Normalised names in DB

--- PaceAcademyApp/models.py
+++ PaceAcademyApp/models.py
@@ -3,17 +3,19 @@ from django.db import models
 # Create your models here.
 
 class PACs(models.Model):
-    name = models.CharField(max_length=100)
+    firstName = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
     email = models.EmailField()
-    department = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Students(models.Model):
-    name = models.CharField(max_length=100)
+    firstName = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
     email = models.EmailField()
-    department = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
     assignedPAC = models.ForeignKey(PACs, on_delete=models.CASCADE, default = 0) #FK linking to pac table
 
     def __str__(self):
