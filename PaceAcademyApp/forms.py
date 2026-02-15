fc5a03da6019d69298e8e727a9377c508c0fb079
Ashley <ashley21_05@hotmail.co.uk>
2026-02-15 11:13:53 +0000

    Fixed error where pac list was being made at import time not at runtime

--- PaceAcademyApp/forms.py
+++ PaceAcademyApp/forms.py
@@ -16,9 +16,13 @@ class AddStudentForm(forms.ModelForm):
     last_name = forms.CharField(max_length=100, label="Last Name")
     email = forms.EmailField(label="Email Address")
     course = forms.CharField(max_length=100, label="Course")
-    assigned_pac = forms.ModelChoiceField(queryset=PACs.objects.all(), label="Assigned PAC")
+    assigned_pac = forms.ModelChoiceField(queryset=PACs.objects.none(), label="Assigned PAC")
 
     class Meta:
         model = Student
         fields = ('first_name', 'last_name', 'email', 'course', 'assigned_pac')
 
+    #This function makes sure that the list of PACs is calculated at runtime not at import time
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['assigned_pac'].queryset = PACs.objects.all()
