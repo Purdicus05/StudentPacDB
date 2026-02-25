1e80d505b59017dbdc3c25fd5e646e7593313d0d
CallumSuffolkUni <callum07467@gmail.com>
2026-02-25 22:35:21 +0000

    final commit

--- PaceAcademyApp/forms.py
+++ PaceAcademyApp/forms.py
@@ -5,35 +5,83 @@ from .models import Students, PACs
 
 # Form to add pacs
 class AddPacForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=100, label="First Name")
-    last_name = forms.CharField(max_length=100, label="Last Name")
-    email = forms.EmailField(label="Email Address")
+    first_name = forms.CharField(
+        max_length=100,
+        label="First Name",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter first name"
+        })
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        label="Last Name",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter last name"
+        })
+    )
+    email = forms.EmailField(
+        label="Email Address",
+        widget=forms.EmailInput(attrs={
+            "class": "form-control",
+            "placeholder": "name@example.com"
+        })
+    )
 
     class Meta:
         model = PACs
         fields = ('first_name', 'last_name', 'email')
-
-        # Required to assign the form-class to each field for rendering
-        widgets = {
-            "first_name": forms.TextInput(attrs={"class": "form-control"}),
-            "last_name": forms.TextInput(attrs={"class": "form-control"}),
-            "email": forms.EmailInput(attrs={"class": "form-control"}),
-        }
+        
 
 #Form to add students
-#Included for the use of a drop-down bar for 'course_choices'
 course_choices = [
-    ("Computing", "Computing"),
-    ("Film Production", "Film Production"),
-    ("Game Development", "Game Development"),
-]
+        ("Computing", "Computing"),
+        ("Film Production", "Film Production"),
+        ("Game Development", "Game Development"),
+    ]
 
 class AddStudentForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=100, label="First Name")
-    last_name = forms.CharField(max_length=100, label="Last Name")
-    email = forms.EmailField(label="Email Address")
-    course = forms.ChoiceField(choices=course_choices, label="Course")
-    assigned_pac = forms.ModelChoiceField(queryset=PACs.objects.none(),empty_label="Select a PAC", label="Assigned PAC")
+    first_name = forms.CharField(
+        max_length=100,
+        label="First Name",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter first name"
+        })
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        label="Last Name",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter last name"
+        })
+    )
+    email = forms.EmailField(
+        label="Email Address",
+        widget=forms.EmailInput(attrs={
+            "class": "form-control",
+            "placeholder": "name@example.com"
+        })
+    )
+    course = forms.ChoiceField(
+        choices=(course_choices), 
+        label="Course",
+        widget=forms.Select(attrs={
+            "class": "form-select",
+            "aria-label": "Select course"
+        })
+    )
+    assigned_pac = forms.ModelChoiceField(
+        queryset=PACs.objects.none(),
+        empty_label="Select a PAC",
+        label="Assigned PAC",
+        widget=forms.Select(attrs={
+            "class": "form-select",
+            "aria-label": "Select PAC"
+        })
+    )
 
     class Meta:
         model = Students
@@ -42,10 +90,9 @@ class AddStudentForm(forms.ModelForm):
     #This function makes sure that the list of PACs is calculated at runtime not at import time
     #This prevents issues with newly added pacs not being shown
     def __init__(self, *args, **kwargs):
+        course_choices = kwargs.pop("course_choices", None)
         super().__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs.update({"class": "form-control", "placeholer": "Enter first name"})
-        self.fields['last_name'].widget.attrs.update({"class": "form-control", "placeholer": "Enter last name"})
-        self.fields['email'].widget.attrs.update({"class": "form-control", "placeholer": "Enter email"})
-        self.fields['course'].widget.attrs.update({"class": "form-select", "arial_label": "Select course"})
-        self.fields['assigned_pac'].widget.attrs.update({"class": "form-select", "arial_label": "Select PAC"})
         self.fields['assigned_pac'].queryset = PACs.objects.all()
+        self.fields['course'].choices = [("", "Select course")] + list(self.fields['course'].choices)
+
+
